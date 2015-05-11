f2ef12db1ee05325fbbf2020821fa6759d78f90e
fabiorochafg <fabiorochafg@gmail.com>
2015-05-11 19:20:56 -0300

    Updating template for Soluti credits

--- files/main.js
+++ files/main.js
@@ -1,7 +1,10 @@
-/* JS Document - Developed by Fabio Rocha (http://fabiorochafg.github.io | fabiorochafg@gmail.com) */
+/* JS Document - Developed by Soluti (http://solutibrasil.com.br | contato@solutibrasil.com.br) */
 
 $(document).ready(function() {
 
-
+	$("#go-to-top").click(function(){
+		$('html, body').animate({scrollTop:0}, 'slow');
+		return false;
+	});
 
 });
