78cf89264eff577ae63b0abe3a23e9159e4fcb6a
Fabio Rocha <fabio.gomes@ifrn.edu.br>
2017-07-20 19:08:16 -0300

    Commit inicial

--- files/main.js
+++ files/main.js
@@ -1,6 +1,18 @@
-/* JS Document - Developed by Soluti (http://solutibrasil.com.br | contato@solutibrasil.com.br) */
+/* JS Document - Developed by Fabio Rocha (http://fabiorochafg.github.io | fabiorochafg@gmail.com) */
 
 $(document).ready(function() {
+
+	/* Menu */
+	$('nav a').on('click', function (e) {
+	    e.preventDefault();
+	    var target = this.hash,
+	    $target = $(target);
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
 
 	$("#go-to-top").click(function(){
 		$('html, body').animate({scrollTop:0}, 'slow');
